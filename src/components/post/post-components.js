69d17ff3f57cc390f0f92617adb9fcb294e9cd0a
nelo <nelokwaz@gmail.com>
2020-03-29 14:51:49 +0100

    completed the structure of the post component

---
+++ src/components/post/post-components.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Card from '../card/card-component';
+
+const Post = ({ postId }) => {
+    const post = [];
+
+    return (
+        <Card>
+            {
+             post ?  (
+                 <div>
+                     <h3>{post.title}</h3>
+             <p>{post.body}</p>
+                 </div>
+             ) : (<p>No post found</p>)
+            }
+        </Card>
+    );
+
+}
