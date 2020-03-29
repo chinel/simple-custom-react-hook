5393114a2686d665249ce74564d0c0bfd49bf79e
nelo <nelokwaz@gmail.com>
2020-03-29 14:56:06 +0100

    created the structure for the user component

---
+++ src/components/user/user-component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Card from '../card/card-component';
+
+const User = ({userId}) => {
+    const user = [];
+
+    return (
+        user ? (
+            <div>
+                <h3>{user.username}</h3>
+        <p>{user.name}</p>
+            </div>
+        ) : (
+            <p>User not found</p>
+        )
+    )
+}
